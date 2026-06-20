cdae596ce64cf7373ae54215f6a8d3af8ebada40
Nathaniel <nathanieldesire8@gmail.com>
2026-06-20 16:58:41 +0100

    commit change

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://my-project-gq9n.onrender.com/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
